58c0dd48c8bf17f74f433c3b541762796597e013
rita.wang <rita.wang@qunar.com>
2016-06-23 19:33:37 +0800

    modify

--- javascript/BubbleSort.js
+++ javascript/BubbleSort.js
@@ -1,3 +1,46 @@
-function bubbleSort () {
+function bubbleSort3(arr, n) {
+    var j, k, mid;
+    var flag;
 
+    flag = n;
+    while (flag > 0) {
+        k = flag;
+        flag = 0;
+        for (j = 1; j < k; j++)
+            if (arr[j - 1] > arr[j]) {
+                mid = arr[j - 1];
+                arr[j - 1] =  arr[j];
+                arr[j] = mid;
+                flag = j;
+            }
+    }
+}
+
+function bubbleSort2(arr, n) {
+    var j, k, mid;
+    var flag;
+
+    k = n || arr.length;
+    flag = true;
+    while (flag) {
+        flag = false;
+        for (j = 1; j < k; j++)
+            if (arr[j - 1] > arr[j]) {
+                mid = arr[j - 1];
+                arr[j - 1] =  arr[j];
+                arr[j] = mid;
+                flag = true;
+            }
+        k--;
+    }
+}
+
+function bubbleSort1() {
+
+}
+
+function swap( arr1, arr2) {
+    var i = arr1;
+    arr1 = arr2;
+    arr2 = arr1;
 }
