a5bb3c1143cbfe3a9697a012ad504304bc59c515
rita.wang <rita.wang@qunar.com>
2017-03-13 19:48:55 +0800

    TLE

--- leetcode/22m-generateParenthesis.js
+++ leetcode/22m-generateParenthesis.js
@@ -4,32 +4,33 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-	var ret = [];
-	var _a = []
+	var _a = [];
+	var keyStr = '';
 	addChar('()', n);
-	for (var i = _a.length - 1; i >= 0; i--) {
-		for (var j = ret.length; j >= 0; j-- ) {
-			if (_a[i] === ret[j]) {
-				break;
-			}
-		}
-		if (j < 0) {
-			ret.push(_a[i]);
-		}
-	}
-	return ret;
+	return _a;
+
 	function addChar(curChars) {
 
 		if (curChars.length === 2 * n) {
-			_a.push(curChars);
+			if ( _a.join(',').indexOf(curChars) === -1){
+				_a.push(curChars);
+			}
 			return;
-		} else {
-			for (var i = 0; i < curChars.length; i ++) {
-				if (curChars[i] === '('){
-					// 左括号之前 和 之后加完整的括号
-					addChar(curChars.slice(0, i) + '()' + curChars.slice(i));
-					addChar(curChars.slice(0, i + 1) + '()' + curChars.slice(i + 1));
-				}
+		}
+
+		for (var i = 0; i < curChars.length; i++) {
+			if (curChars[i] === '('){
+				// 左括号之前 和 之后加完整的括号
+				var lAddStr = curChars.slice(0, i) + '()' + curChars.slice(i);
+				var rAddStr = curChars.slice(0, i + 1) + '()' + curChars.slice(i + 1);
+				// if (keyStr.indexOf(lAddStr) === -1) {
+				// 	keyStr += lAddStr + ',';
+					addChar(lAddStr);
+				// }
+				// if (keyStr.indexOf(rAddStr) === -1) {
+					// keyStr += rAddStr + ',';
+					addChar(rAddStr);
+				// }
 			}
 		}
 	}
