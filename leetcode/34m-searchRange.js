fe683efd5b4594472359d91bedaa35df1f08f3d9
rita.wang <rita.wang@qunar.com>
2017-03-23 13:21:00 +0800

    优化

--- leetcode/34m-searchRange.js
+++ leetcode/34m-searchRange.js
@@ -11,24 +11,30 @@ var searchRange = function(nums, target) {
     var ll = 0;
     var lr = nums.length - 1;
     while (ll <= lr) {
-        var m = (ll + lr) / 2;
+        var m = parseInt((ll + lr) / 2);
         if(nums[m] < target) {
             ll = m + 1;
         } else {
             lr = m - 1;
         }
     }
+	if (ll >= nums.length) {
+		return res;
+	}
     var rl = 0;
     var rr = nums.length - 1;
     while(rl <= rr) {
-        var m = (rl + rr) / 2;
+        var m = parseInt((rl + rr) / 2);
         if(nums[m] <= target) {
             rl = m + 1;
         } else {
             rr = m - 1;
         }
     }
-    if(ll<=rr)
+	if (rr < 0) {
+		return res;
+	}
+    if(ll <= rr)
     {
         res[0] = ll;
         res[1] = rr;
