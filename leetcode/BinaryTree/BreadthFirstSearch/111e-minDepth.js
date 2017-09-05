0126788be8f4938f0702ac6a2a27bff35afdf610
rita.wang <rita.wang@qunar.com>
2017-09-05 14:18:33 +0800

    BT min depth

--- leetcode/BinaryTree/BreadthFirstSearch/111e-minDepth.js
+++ leetcode/BinaryTree/BreadthFirstSearch/111e-minDepth.js
@@ -14,6 +14,36 @@ var minDepth = function(root) {
 		return 0;
 	}
 	let queue = [];
+	let levelQueue = [];
+	levelQueue.push(root);
+	let depth = 0;
+	while (levelQueue.length) {
+		depth++;
+		queue = levelQueue.concat();
+		levelQueue = [];
+		while (queue.length) {
+			const curNode = queue.shift();
+			if (!curNode.left && !curNode.right) {
+				return depth;
+			}
+			if (curNode.left) {
+				levelQueue.push(curNode.left);
+			}
+			if (curNode.right) {
+				levelQueue.push(curNode.right);
+			}
+		}
+	}
+	return depth;
+};
+
+
+// 超时
+var minDepth = function(root) {
+	if (!root) {
+		return 0;
+	}
+	let queue = [];
 	queue.push(root);
 	let depth = 0;
 	let curLevelNodeSum = 1;
