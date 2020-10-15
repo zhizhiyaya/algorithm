935bf0c647761b89c5cf410dc89c0b8550fa6041
王洁 <825782615@qq.com>
2020-10-15 15:07:34 +0800

    Update 1290e-getDecimalValue.js

--- leetcode/linkedList/1290e-getDecimalValue.js
+++ leetcode/linkedList/1290e-getDecimalValue.js
@@ -6,6 +6,7 @@
  * }
  */
 /**
+ * @desc 二进制转10进制
  * @param {ListNode} head
  * @return {number}
  */
@@ -23,9 +24,31 @@ var getDecimalValue = function(head) {
 var getDecimalValue = function(head) {
     var res = 0;
     while (head != null) {
-        res = (res << 1) | head.val;
+        res = (res << 1) | head.val; // 左移一位 相当于*2，按位或， 0001 即 + 1
         head = head.next;
     }
     return res;
 };
 
+
+// Example 1:
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+// Example 2:
+
+// Input: head = [0]
+// Output: 0
+// Example 3:
+
+// Input: head = [1]
+// Output: 1
+// Example 4:
+
+// Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+// Output: 18880
+// Example 5:
+
+// Input: head = [0,0]
+// Output: 0
