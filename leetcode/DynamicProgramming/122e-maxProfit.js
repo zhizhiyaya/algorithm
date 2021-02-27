31a21baf0ccd952c97069f0fb8ab737dbdee9788
王洁 <825782615@qq.com>
2021-02-27 10:50:33 +0800

    Create 122e-maxProfit.js

---
+++ leetcode/DynamicProgramming/122e-maxProfit.js
@@ -0,0 +1,22 @@
+/**
+ * @desc 多次买卖，只要后者大于前者，就可以进行一次买卖。同一天可以同时买卖(当前数最多买1次卖1次)
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    const length = prices.length;
+    for(let i = 0; i < length; i++) {
+        if (prices[i+1] - prices[i] > 0) {
+            max += prices[i+1] - prices[i];
+        }
+    }
+    return max;
+};
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// [7,1,5,3,4,6]
+// Output: 7
+// 
