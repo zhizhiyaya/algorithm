7eed4b8d5bdf7ad209901fbad903ffe37793c2b8
王洁 <825782615@qq.com>
2021-03-26 19:23:24 +0800

    Update promise.js

--- collection/promise.js
+++ collection/promise.js
@@ -10,6 +10,7 @@ function Promise(executor) {
     this.status = 'pending';
     this.queue = [];
     this.isPromise = true;
+    this.parentPromise = null;
     if (isFunction(executor)) {
         executor.call(this, this.resolve, this.reject);
     }
@@ -18,8 +19,14 @@ function Promise(executor) {
 var fn = Promise.prototype;
 
 fn.setFinalStatus = function setFinalStatus(status) {
+    var rootPromise = this;
+    while (rootPromise.parentPromise) {
+        rootPromise = rootPromise.parentPromise;
+    }
     this.status = status;
-    return this;
+    rootPromise.status = status;
+
+    return rootPromise;
 }
 
 fn.then = function then(onFulfilled, onRejected) {
@@ -28,14 +35,10 @@ fn.then = function then(onFulfilled, onRejected) {
     if (isFnForDone || isFnForFail) {
         switch (this.status) {
             case 'pending':
-                var obj = {};
-                if (isFnForDone) {
-                    obj.onFulfilled = onFulfilled;
-                }
-                if (isFnForFail) {
-                    obj.onRejected = onRejected;
-                }
-                this.queue.push(obj);
+                this.queue.push({
+                    onFulfilled,
+                    onRejected
+                });
                 break;
             case 'fulfilled':
                 onFulfilled();
@@ -53,21 +56,41 @@ fn.then = function then(onFulfilled, onRejected) {
 fn.resolve = function resolve() {
     var next,
         nextDone,
-        args,
+        args
         ret;
-
-    next = this.queue.shift();
-    // if (next && this.status === '')
+    args = Array.prototype.slice.call(arguments).concat(this);
+    if (this.status === 'pending') {
+        next = this.queue.shift();
+        while(next && next.onFulfilled) {
+            try {
+                args = next.onFulfilled(args);
+                if (args && args.isPromise) {
+                    args.queue = args.queue.concat(this.queue);
+                    args.parentPromise = this; 
+                    return;
+                }
+            } catch(e) {
+                this.reject(e);
+            }
+            next = this.queue.shift();
+        }
+        this.status = 'fulfilled';
+    } else {
+        console.log('this promise has been resolved/rejected!');
+    }
 };
 
 fn.reject = function resolve() {
     var next,
-        nextDone,
-        args,
-        ret;
-
+        args;
     next = this.queue.shift();
-    if (next && this.status === '')
+    if (next && next.onRejected) {
+        args = Array.prototype.slice.call(arguments).concat(this);
+        next.onRejected(args);
+    }
+    this.queue = [];
+    rootPromise = this.setFinalStatus('rejected');
+    rootPromise && (rootPromise.queue = []);
 };
 
 function toString(obj) {
@@ -76,4 +99,51 @@ function toString(obj) {
 
 function isFunction(obj) {
 	return toString(obj) === 'object Function';
+}
+
+
+
+// var a = new Promise((resolve, reject) => {
+//     console.log(111);
+//     resolve();
+// }).then(res=> {
+//     return new Promise(() => {
+//         console.log(222);
+//     });
+// }).then(() => {
+//     console.log(333);
+// })
+// // 111
+// // 222
+// // undefined
+// // a pending
+
+
+function p(type){
+    return new Promise((resolve,reject)=>{
+        if (type=='yes') {
+            setTimeout(()=>{
+                resolve('resolved');
+            },5000);
+        }else{
+            setTimeout(()=>{
+                reject('rejected');
+            },5000);
+        }
+    })
+}
+
+function resolve(){
+    console.log('before:',this)
+    return p('yes').then(res=>{
+        console.log('res',this);
+        console.log(res);
+    }).catch(err=>{
+        console.log('err',this);
+        console.log(err);
+    })
+}
+function Aa() {
+    this.p = resolve();
+    // Pormise this 指向 window
 }
